f6dce227537f5e086bb10662f34d225275151ea6
Evenstarr <q23FtL91>
2020-09-10 12:37:10 +0300

    Задача 4 без индексов.

--- 4_1.py
+++ 4_1.py
@@ -2,6 +2,7 @@
 
 n = input("Введите целое положительное число: ")
 
+'''
 i = 0
 
 maximum = int(n[0])
@@ -9,5 +10,15 @@ while i < len(n):
     if int(n[i]) > maximum:
         maximum = int(n[i])
     i += 1
+'''
+res = int(n)
+tmp_max = 0
+maximum = 0;
+
+while res > 10:
+    tmp_max = res % 10;
+    if tmp_max > maximum:
+        maximum = tmp_max;
+    res = res // 10;
 
 print(f"Самая большая цифра в числе - {maximum}")
