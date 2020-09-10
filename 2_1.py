669ce7c1b40326e4104f6b80c95ff1b9ed0b519c
Evenstarr <q23FtL91>
2020-09-10 11:04:34 +0300

    Вот оказывается как можно f строки форматировать

--- 2_1.py
+++ 2_1.py
@@ -6,11 +6,13 @@ hours = sec // 3600
 minutes = (sec - hours * 3600) // 60
 seconds = sec - hours * 3600 - minutes * 60
 
+'''
 if hours < 10:
     hours = "0" + str(hours)
 if minutes < 10:
     minutes = "0" + str(minutes)
 if seconds < 10:
     seconds = "0" + str(seconds)
+'''
 
-print(f'Введенное время - {hours}:{minutes}:{seconds}')
+print(f'Введенное время - {hours:02d}:{minutes:02d}:{seconds:02d}')
