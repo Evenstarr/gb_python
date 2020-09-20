e8b297d087620bbcbcb237ba623550a7d474786f
Evenstarr <alhe@yandex.ru>
2020-09-21 00:39:01 +0300

    initial 4th

---
+++ 4_5.py
@@ -0,0 +1,16 @@
+# 5. Реализовать формирование списка, используя функцию range() и возможности генератора.
+# В список должны войти четные числа от 100 до 1000 (включая границы).
+# Необходимо получить результат вычисления произведения всех элементов списка.
+# Подсказка: использовать функцию reduce().
+
+
+from functools import reduce
+
+
+def multiply_list(prev_el, el):
+    return prev_el * el
+
+
+initial_list = [x for x in range(100, 1001) if x % 2 == 0]
+
+print(reduce(multiply_list, initial_list))
