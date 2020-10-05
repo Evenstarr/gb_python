b8cc173bfeaa53da4ecc53179c6e64c78da1aca3
Evenstarr <alhe@yandex.ru>
2020-10-05 12:17:12 +0300

    Lesson 8 8_4-6 format warehouse changed + test data commented

--- 8_4-6.py
+++ 8_4-6.py
@@ -13,38 +13,30 @@ class Warehouse:
     def add_product(self, product, quantity):
         try:
             quantity = int(quantity)
-            for _ in range(quantity):
-                self.products.append(product)
+            self.products.append([product, quantity])
         except (TypeError, ValueError):
             print("ERROR - Cant add - quantity not integer")
 
     def remove_product(self, product, quantity):
-        cnt = 0
-
         try:
             quantity = int(quantity)
 
-            for n in range(quantity):
-                for ix, item in enumerate(self.products):
-                    # Я знаю, что это плохо) И что у нас могут быть модели, имеющие те же самые параметры,
-                    # отличающиеся аргументами классов. Тут надо думать.
-                    if item.barcode == product.barcode:
-                        del self.products[ix]
-                        cnt += 1
-                        break
-
-            if cnt < quantity:
-                print(f"На складе нет {quantity} '{product.type} {product.name}', отдали последнее - только {cnt}")
-            else:
-                print(f"Отдали в другие подразделения {quantity} '{product.type} {product.name}'")
+            for idx, item in enumerate(self.products):
+                if item[0].barcode == product.barcode:
+                    if quantity >= item[1]:
+                        print(f"отдали последние - {item[1]} '{product.type} {product.name}'")
+                        # раз не осталось таких - убираем
+                        del self.products[idx]
+                    else:
+                        print(f"Отдали в другие подразделения {quantity} '{product.type} {product.name}'")
+                        self.products[idx] = [item[0], item[1] - quantity]
         except (TypeError, ValueError):
             print("ERROR - Cant remove - quantity non integer")
 
     def __str__(self):
         s = ''
         for pr in self.products:
-            s += "{}\n".format(pr)
-            print("{}\n".format(pr))
+            s += "{} ({})\n".format(pr[0], pr[1])
         return s
 
 
@@ -67,7 +59,8 @@ class Scanner(OfficeEquipment):
         self.type = "Сканер"
 
     def __str__(self):
-        return f"Что это - {self.type}, штрихкод - {self.barcode}, название - {self.name}, цена - {self.price}, размер - {self.size}, фото - {self.is_photo}"
+        return f"Что это - {self.type}, штрихкод - {self.barcode}, название - {self.name}, цена - {self.price}, " \
+               f"размер - {self.size}, фото - {self.is_photo}"
 
 
 class Monitor(OfficeEquipment):
@@ -89,20 +82,27 @@ data_printer = input("Введите информацию о принтере ч
 
 printer = Printer(*data_printer)
 printer_quantity = input("Количество таких принтеров: ")
+# printer = Printer(123, 50, 50, 'Canon', True, True)
+# printer_quantity = 5
 wh.add_product(printer, printer_quantity)
-
+#
 data_scanner = input("Введите информацию о сканере через пробел - штрихкод, "
                      "размер, цена, название, фотопринтер(булево): ").split()
 scanner_quantity = input("Количество таких сканеров: ")
 
 scanner = Scanner(*data_scanner)
 
+# scanner = Scanner(456, 100, 100, 'Nikon', True)
+# scanner_quantity = 2
 wh.add_product(scanner, scanner_quantity)
-
-
+#
+#
 data_monitor = input("Введите информацию о мониторе через пробел - штрихкод, тип, размер, цена, название: ").split()
 monitor_quantity = input("Количество таких мониторов: ")
 monitor = Monitor(*data_monitor)
+
+# monitor = Monitor(789, 'LCD', 20, 200, 'Samsung', False)
+# monitor_quantity = 3
 wh.add_product(monitor, monitor_quantity)
 
 print(wh)
